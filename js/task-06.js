72032eed9cdd58494a743caf67c643d05e2c026e
Anna <80330820+AnnaKilchytska@users.noreply.github.com>
2022-12-16 19:59:30 +0200

    fix the problem with classes

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,12 @@
 const inputField = document.querySelector("[data-length='6']");
 
 inputField.addEventListener("blur", (event) => {
-  if (event.currentTarget.value.length === 6) {
+  if (event.currentTarget.value.length === +inputField.dataset.length) {
+    inputField.classList.remove("invalid");
     inputField.classList.add("valid");
+ 
   } else {
+    inputField.classList.remove("valid");
     inputField.classList.add("invalid");
   }
 });
